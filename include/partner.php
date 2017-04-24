7996526d77feda543fe1ecffd436414fc630315f
Admin <git@webgk.ru>
2017-04-24 10:21:11 +0300

    local changes

--- include/partner.php
+++ include/partner.php
@@ -1,4 +1,6 @@
+<br>
 Авито<br>
  Из рук в руки<br>
  AutoRenter<br>
  Гидпрокат<br>
+ <noindex><a href="http://icra.rentals/"><img width="100" alt="ICRA" target="_blank" src="http://icra.rentals/wp-content/uploads/2013/12/icra.png" height="50" title="Международная Ассоциация Автопрокатных Компаний"> </a></noindex>
